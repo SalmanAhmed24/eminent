f5068824586cabd8e319798c38ce14fc4a23b52d
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-11-15 19:53:39 +0500

    added new subject

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -9,7 +9,7 @@ export default function(req, res) {
 	var mailOptions = {
 		from: 'support@eminentconsultingandservices.com',
 		to: 'support@eminentconsultingandservices.com',
-		subject: 'Contact Form',
+		subject: 'Contact Form Submitted from website',
 		html: `<h3>Name:</h3><p>${req.body.name}</p><h3>Email:</h3><p>${req.body.email}</p><h3>Phone:</h3><p>${req.body
 			.phone}</p><h3>Service:</h3><p>${req.body.service}</p><h3>Message:</h3><p>${req.body.message}</p>`
 	};
