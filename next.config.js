cf18b2fabdf8f6de898d071f6761902ae0d531d8
Salman Ahmed Abbasis <40638940+SalmanAhmed24@users.noreply.github.com>
2022-11-16 08:07:20 +0500

    added new api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+	reactStrictMode: true
+};
+module.exports = {
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://www.eminentconsultingandservices.com/:path*'
+			}
+		];
+	}
+};
+module.exports = nextConfig;
